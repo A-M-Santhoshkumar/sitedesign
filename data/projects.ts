f88e776707ee356d8c9cc7be032da9446d3c7837
santhoshkumar-a-m <amsanthoshkumar2@gmail.com>
2026-04-01 12:48:14 +0530

    service

--- data/projects.ts
+++ data/projects.ts
@@ -12,10 +12,10 @@ export const projectsData: ProjectType[] = [
   { id: 4,  name: "Automind Infotech",               tool: "Html",                  link: "https://automindinfotech.com/",               img: "/images/Project/automindinfotech.png" },
   { id: 5,  name: "Sk Greentech",                    tool: "Codeigniter (PHP)",     link: "https://skgreentech.in/",                     img: "/images/Project/skgreentech.png" },
    { id: 6, name: "Space Group",                      tool: "Wordpress",             link: "https://spacgroup.com/",                    img: "/images/Project/spacgroup.png" },
-   { id: 7, name: "JKKN DENTAL COLLEGE & HOSPITAL", tool: "Wordpress",             link: "https://dental.jkkn.ac.in/",                 img: "/images/Project/dental.png" },
-  { id: 8,  name: "Grill Nights",                    tool: "Html, Css, Bootstrap",  link: "https://grillnights.in/",                     img: "/images/Project/grillnights.png" },
-  { id: 9, name: "React Ecommerce",                 tool: "React, Tailwind",       link: "https://sitedesign-ecommerce.vercel.app/",    img: "/images/Project/ecommerce.png" },
-
+   { id: 7, name: "Mak Creation",                     tool: "Wordpress & PHP",       link: "https://makcreation.in/",                     img: "/images/Project/makcreation.png" },
+   { id: 8, name: "JKKN DENTAL COLLEGE & HOSPITAL", tool: "Wordpress",             link: "https://dental.jkkn.ac.in/",                 img: "/images/Project/dental.png" },
+  { id: 9,  name: "Grill Nights",                    tool: "Html, Css, Bootstrap",  link: "https://grillnights.in/",                     img: "/images/Project/grillnights.png" },
+  { id: 10, name: "React Ecommerce",                 tool: "React, Tailwind",       link: "https://sitedesign-ecommerce.vercel.app/",    img: "/images/Project/ecommerce.png" },
 ];
 // Dynamic list of unique tools (order preserved)
 export const toolCategories: { label: string; key: string }[] = [
