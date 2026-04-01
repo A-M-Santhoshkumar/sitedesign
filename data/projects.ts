5bbcef1246d7f9ab16bc71c022e44124680a8348
santhoshkumar-a-m <amsanthoshkumar2@gmail.com>
2026-04-01 12:21:06 +0530

    gmail

--- data/projects.ts
+++ data/projects.ts
@@ -8,13 +8,14 @@ export type ProjectType = {
 
 export const projectsData: ProjectType[] = [
   { id: 1,  name: "THE LABEL by MONA",              tool: "Shopify",               link: "https://thelabelbymona.com/",                img: "/images/Project/thelabelbymona.png" },
-  { id: 3,  name: "JKKN DENTAL COLLEGE & HOSPITAL", tool: "Wordpress",             link: "https://dental.jkkn.ac.in/",                 img: "/images/Project/dental.png" },
+  { id: 3,  name: "Maruthi Prints",                 tool: "Wordpress & PHP",         link: "https://maruthiprints.com/",                img: "/images/Project/maruthiprints.png" },
   { id: 4,  name: "Automind Infotech",               tool: "Html",                  link: "https://automindinfotech.com/",               img: "/images/Project/automindinfotech.png" },
   { id: 5,  name: "Sk Greentech",                    tool: "Codeigniter (PHP)",     link: "https://skgreentech.in/",                     img: "/images/Project/skgreentech.png" },
-  { id: 6,  name: "Grill Nights",                    tool: "Html, Css, Bootstrap",  link: "https://grillnights.in/",                     img: "/images/Project/grillnights.png" },
-  { id: 7, name: "React Ecommerce",                 tool: "React, Tailwind",       link: "https://sitedesign-ecommerce.vercel.app/",    img: "/images/Project/ecommerce.png" },
-  { id: 8, name: "Spotify Clone",                   tool: "React, Tailwind",       link: "https://saitify-spotify-clone.vercel.app/",   img: "/images/Project/spotify_clone.png" },
-  { id: 9, name: "React Portfolio",                 tool: "Next.js, Tailwind",     link: "https://www.sitedesign.in/",                  img: "/images/Project/react_portfolio.png" },
+   { id: 6, name: "Space Group",                      tool: "Wordpress",             link: "https://spacgroup.com/",                    img: "/images/Project/spacgroup.png" },
+   { id: 7, name: "JKKN DENTAL COLLEGE & HOSPITAL", tool: "Wordpress",             link: "https://dental.jkkn.ac.in/",                 img: "/images/Project/dental.png" },
+  { id: 8,  name: "Grill Nights",                    tool: "Html, Css, Bootstrap",  link: "https://grillnights.in/",                     img: "/images/Project/grillnights.png" },
+  { id: 9, name: "React Ecommerce",                 tool: "React, Tailwind",       link: "https://sitedesign-ecommerce.vercel.app/",    img: "/images/Project/ecommerce.png" },
+
 ];
 // Dynamic list of unique tools (order preserved)
 export const toolCategories: { label: string; key: string }[] = [
